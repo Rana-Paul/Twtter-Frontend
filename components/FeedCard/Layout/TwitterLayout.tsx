af1d7df1bfd9c591d0da747120cc97083cf4a72e
Rana-Mapscrew <ranapaul741223@gmail.com>
2023-10-08 21:00:59 +0530

    user_recommendation

--- components/FeedCard/Layout/TwitterLayout.tsx
+++ components/FeedCard/Layout/TwitterLayout.tsx
@@ -28,60 +28,58 @@ interface TwitterLayoutProps {
 interface TwitterSidebarButton {
   title: string;
   icon: React.ReactNode;
-  link: string
+  link: string;
 }
 
-
 const Twitterlayout: React.FC<TwitterLayoutProps> = (props) => {
-
   const { user } = useCurrentUser();
   const queryClient = useQueryClient();
 
-  
-
-  const sideBarManuItem:TwitterSidebarButton[] = useMemo(() => [
-    {
-      title: "Home",
-      icon: <BiHomeCircle />,
-      link: '/'
-    },
-    {
-      title: "Explore",
-      icon: <BiHash />,
-      link: '/'
-    },
-    {
-      title: "Notification",
-      icon: <BsBell />,
-      link: '/'
-    },
-    {
-      title: "Messages",
-      icon: <BsEnvelope />,
-      link: '/'
-    },
-    {
-      title: "Bookmarks",
-      icon: <BsBookmark />,
-      link: '/'
-    },
-    {
-      title: "Twitter Blue",
-      icon: <BiMoney />,
-      link: '/'
-    },
-    {
-      title: "Profile",
-      icon: <BiUser />,
-      link: `/${user?.id}`
-    },
-    {
-      title: "More Options",
-      icon: <SlOptions />,
-      link: '/'
-    },
-  ], [user?.id]);
-
+  const sideBarManuItem: TwitterSidebarButton[] = useMemo(
+    () => [
+      {
+        title: "Home",
+        icon: <BiHomeCircle />,
+        link: "/",
+      },
+      {
+        title: "Explore",
+        icon: <BiHash />,
+        link: "/",
+      },
+      {
+        title: "Notification",
+        icon: <BsBell />,
+        link: "/",
+      },
+      {
+        title: "Messages",
+        icon: <BsEnvelope />,
+        link: "/",
+      },
+      {
+        title: "Bookmarks",
+        icon: <BsBookmark />,
+        link: "/",
+      },
+      {
+        title: "Twitter Blue",
+        icon: <BiMoney />,
+        link: "/",
+      },
+      {
+        title: "Profile",
+        icon: <BiUser />,
+        link: `/${user?.id}`,
+      },
+      {
+        title: "More Options",
+        icon: <SlOptions />,
+        link: "/",
+      },
+    ],
+    [user?.id]
+  );
 
   const handelLoginWithGoogle = useCallback(
     async (cred: CredentialResponse) => {
@@ -104,39 +102,38 @@ const Twitterlayout: React.FC<TwitterLayoutProps> = (props) => {
     [queryClient]
   );
 
-
   return (
     <div>
       <div className="grid grid-cols-12 h-screen w-screen sm:px-56">
         <div className="col-span-2 pt-1 sm:col-span-3 flex sm:justify-end p-4 relative">
           <div>
-          <div className="text-2xl hover:bg-gray-600 rounded-full p-2 h-fit w-fit cursor-pointer transition-all">
-            <BsTwitter />
-          </div>
-
-          <div className="mt-1 text-xl font-semibold pr-3">
-            <ul>
-              {sideBarManuItem.map((item) => (
-                <li
-                  key={item.title}
-                  
-                >
-                  <Link className="flex justify-start items-center gap-3 hover:bg-gray-600 rounded-full px-3 py-2 w-fit cursor-pointer mt-2 transition-all" href={item.link}>
-                  <span className="text-3xl">{item.icon}</span>
-                  <span className="hidden sm:inline">{item.title}</span>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-            <div className="mt-5 px-3">
-              <button className="hidden sm:block bg-[#1d9bf0] font-semibold text-lg py-2 px-4 rounded-full w-full ">
-                Tweet
-              </button>
-              <button className="block sm:hidden bg-[#1d9bf0] font-semibold text-lg py-2 px-4 rounded-full w-full justify-center ">
-                <BsTwitter />
-              </button>
+            <div className="text-2xl hover:bg-gray-600 rounded-full p-2 h-fit w-fit cursor-pointer transition-all">
+              <BsTwitter />
             </div>
-          </div>
+
+            <div className="mt-1 text-xl font-semibold pr-3">
+              <ul>
+                {sideBarManuItem.map((item) => (
+                  <li key={item.title}>
+                    <Link
+                      className="flex justify-start items-center gap-3 hover:bg-gray-600 rounded-full px-3 py-2 w-fit cursor-pointer mt-2 transition-all"
+                      href={item.link}
+                    >
+                      <span className="text-3xl">{item.icon}</span>
+                      <span className="hidden sm:inline">{item.title}</span>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+              <div className="mt-5 px-3">
+                <button className="hidden sm:block bg-[#1d9bf0] font-semibold text-lg py-2 px-4 rounded-full w-full ">
+                  Tweet
+                </button>
+                <button className="block sm:hidden bg-[#1d9bf0] font-semibold text-lg py-2 px-4 rounded-full w-full justify-center ">
+                  <BsTwitter />
+                </button>
+              </div>
+            </div>
           </div>
           <div className="absolute bottom-5 flex gap-2 items-center bg-slate-800 px-3 py-2 rounded-full">
             {user && user.profileImageURL && (
@@ -148,6 +145,7 @@ const Twitterlayout: React.FC<TwitterLayoutProps> = (props) => {
                 width={50}
               />
             )}
+
             <div className="hidden sm:block">
               <h3 className=" text-xl">{user?.firstName}</h3>
               <h3 className=" text-xl">{user?.lastName}</h3>
@@ -158,10 +156,31 @@ const Twitterlayout: React.FC<TwitterLayoutProps> = (props) => {
           {props.children}
         </div>
         <div className="col-span-0 sm:col-span-3 p-5">
-          {!user && (
+          {!user ? (
             <div className="p-5 bg-slate-700 rounded-lg">
               <h1 className="my-2 text-2xl">New to Twitter</h1>
               <GoogleLogin onSuccess={handelLoginWithGoogle} />
+            </div>
+          ) : (
+            <div className="px-4 py-2 bg-slate-800 rounded-lg mt-4">
+              <h1 className="my-2 text-base mb-5">User you may know</h1>
+              {user?.recommendedUsers?.map((el) => (
+                <div className="flex items-center gap-3 mt-2" key={el?.id}>
+                  {el?.profileImageURL && (
+                    <Image
+                      src={el.profileImageURL}
+                      alt="User Image"
+                      className="rounded-full"
+                      height={60}
+                      width={60}
+                    />
+                  )}
+                  <div className="text-lg">
+                    <div>{el?.firstName} {el?.lastName}</div>
+                    <Link href={`/${el?.id}`} className="bg-white text-black text-sm px-3 py-1 w-full rounded-lg">View</Link>
+                  </div>
+                </div>
+              ))}
             </div>
           )}
         </div>
